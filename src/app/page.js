30cb5fac95bf74ec4a2ec1adbd72afca8ceeb2ec
jeyaseelan <jeyaseelan.work@gmail.com>
2024-06-29 12:10:41 +0530

    Fix protected route

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-"use client"
-
-import ProductedRoute from "@/components/ProductedRoute";
+import Link from "next/link";
 import Section from "../components/Section";
 
 import styles from "./page.module.css";
@@ -8,11 +6,10 @@ import styles from "./page.module.css";
 const Home = () => {
   return (
     <Section>
-      <h1>Home</h1>      
+      <h1>Home</h1>
+      <Link href={"/about"}>About</Link>      
     </Section>
   );
 }
 
-const d = ProductedRoute(Home);
-// export default Home;
-export default d;
+export default Home;
--- src/components/ProductedRoute/index.js
+++ src/components/ProductedRoute/index.js
@@ -1,19 +1,14 @@
-import { redirect } from "next/navigation";
+"use client";
+
 import { getCookie } from "@/helpers/cookies";
 
 const productedRoute = (WrappedComponent) => {
   if (!getCookie("auth_login")) {
-    redirect("/auth");
+    window.location.replace("/auth");
+    return <></>;
   }
 
-  const ProductedRoute = (props) => {
-    return <WrappedComponent {...props} />;
-  };
-
-  ProductedRoute.displayName = `productedRoute(${
-    WrappedComponent.displayName || WrappedComponent.name
-  })`;
-  return ProductedRoute;
+  return WrappedComponent;
 };
 
 export default productedRoute;
