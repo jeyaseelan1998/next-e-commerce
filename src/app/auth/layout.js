2105aa45a453f9dd1d16502efdfa4ab7e1a54c11
jeyaseelan <jeyaseelan.work@gmail.com>
2024-07-01 16:18:33 +0530

    Add Auth api (login, logout, register)

---
+++ src/app/auth/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Ecommerce Login / Register",
+  description: "Please login or register to continue shopping",
+};
+
+export default function AuthLayout({ children }) {
+  return <>{children}</>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
 import Link from "next/link";
+
 import Section from "../components/Section";
+import Logout from "@/components/Logout";
 
 import styles from "./page.module.css";
 
 const Home = () => {
   return (
     <Section>
+      <nav className={styles.nav}>
+        <Link href={"/about"}>About</Link>
+        <Logout />
+      </nav>
       <h1>Home</h1>
-      <Link href={"/about"}>About</Link>      
     </Section>
   );
-}
+};
 
 export default Home;
